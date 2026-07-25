1f85b619db9ba0ced46c1423cec04a3c3a7053ac
Robel Yoseph <154006638+RobelMessi@users.noreply.github.com>
2026-07-25 14:45:50 -0400

    Basic arithmetic in numpy

--- numpy_practice/slicing.py
+++ numpy_practice/slicing.py
@@ -7,18 +7,18 @@ array = np.array([[1, 2, 3 ,4],
 #array[start:end:step]
 
 
-# print(array[0]) #first row 
-# print(array[-1]) #last row
-# print(array[0:3]) #first three rows
-# print(array[0:4:2]) #every second row
-# print(array[::-1]) #return the array in reverse 
+print(array[0]) #first row 
+print(array[-1]) #last row
+print(array[0:3]) #first three rows
+print(array[0:4:2]) #every second row
+print(array[::-1]) #return the array in reverse 
 
 #Column Selection
-# print(array[0, 0]) #first row first column
-# print(array[:, 0]) #all rows access column 0
-# print(array[:, -1]) #last column
-# print(array[:4, :3]) #first three columns
-# print(array[:, 1:]) #columns two to four
+print(array[0, 0]) #first row first column
+print(array[:, 0]) #all rows access column 0
+print(array[:, -1]) #last column
+print(array[:4, :3]) #first three columns
+print(array[:, 1:]) #columns two to four
 
 #Row and Column Selectiom
 print(array[0:2, 0:2]) #first two rows, first two columns
