62eebbc4d083751fee5e91fd46120ddf652b083a
Robel Yoseph <154006638+RobelMessi@users.noreply.github.com>
2026-07-25 17:22:39 -0400

    Basic filtering in NumPy

---
+++ numpy_practice/filtering.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#Filtering = Refers to the process of selecting elements from an array that match a given condition
+
+ages = np.array([[21, 17, 19, 20, 16, 30, 18, 65], 
+                 [39, 22, 15, 99, 18, 19, 20, 21]])
+
+teenagers = ages[ages<18]
+print(teenagers) #list of teenagers
+adults = ages[(ages >= 18) & (ages <65)]
+print(adults)
+seniors = ages[ages >= 65]
+print(seniors)
+evens = ages[ages % 2 ==0]
+print(evens)
+
+adults = np.where(ages >=18, ages, 0)
+print(adults)
